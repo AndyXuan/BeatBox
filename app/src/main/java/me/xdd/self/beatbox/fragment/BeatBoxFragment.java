badc502a9fa8b692359efaa32a88a1e8d2bf463f
xuandongdong <xuandong@juxiangfen.com>
2019-05-06 18:37:37 +0800

    Initial commit

---
+++ app/src/main/java/me/xdd/self/beatbox/fragment/BeatBoxFragment.java
@@ -0,0 +1,10 @@
+package me.xdd.self.beatbox.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newIntent(){
+        return new BeatBoxFragment();
+    }
+
+}
